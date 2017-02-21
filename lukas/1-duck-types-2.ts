908d3cb4d6dbe13e4a03e6aa6b7e966ace7a46e3
Lukas Elmer <lukas.elmer@renuo.ch>
2017-02-21 11:26:25 +0100

    Duck types

---
+++ lukas/1-duck-types-2.ts
@@ -0,0 +1,27 @@
+export class Duck {
+  constructor(public name: string) {}
+
+  sound() {
+    console.log(`Quack from ${this.name} Duck`);
+  }
+}
+
+export function makeSound(duck: Duck) {
+  duck.sound();
+}
+
+const duck = new Duck('Donald');
+makeSound(duck);
+
+export class Bunny {
+  constructor(public name: string) {}
+
+  // unused method?
+  sound() {
+    console.log(`Hop from ${this.name} Bunny`);
+  }
+}
+
+const bunny = new Bunny('Bugs');
+// this works, because the Bunny "interface" is the same as the Duck "interface"
+makeSound(bunny);
